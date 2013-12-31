f3dab00b397c6483a3453d4804e5e586f7880261
Sha Liu <leonliusha@gmail.com>
2013-12-31 15:31:45 +0800

    add a entity package.

---
+++ CareerLog/src/main/java/com/careerlog/entity/User.java
@@ -0,0 +1,15 @@
+package com.careerlog.entity;
+
+import java.util.Date;  
+
+public class User {  
+    private int userId;  
+    private String code;  
+    private String password;  
+    private String email;  
+    private String state;  
+    private String createBy;  
+    private Date createDatetime;  
+    private String modifyBy;  
+    private Date modifyDatetime; 
+}
